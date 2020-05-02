83645d1278690e40fab6f28f734fbf8d7e0f569e
velengel <koutyan_dora_ouchi@yahoo.co.jp>
2020-05-02 08:29:24 +0000

    step4 totyu

--- 9cc.c
+++ 9cc.c
@@ -87,6 +87,23 @@ Token *tokenize(char *p){
 	return head.next;
 }
 
+//step4
+char *user_input;
+
+void error_at(char *loc, char *fmt, ...){
+	va_list ap;
+	va_start(ap, fmt);
+
+	int pos = loc - user_input;
+	fprintf(stderr, "%s\n", user_input);
+	fprintf(stderr, "%*s", pos, "");
+	fprintf(stderr, "^ ");
+	vfprintf(stderr, fmt, ap);
+	fprintf(stderr, "\n");
+	exit(1);
+}
+
+//step4 end
 int main(int argc, char **argv){
 	if(argc != 2){
 		error("not match the number of arguments");
