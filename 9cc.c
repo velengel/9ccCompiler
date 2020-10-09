d70ecaddb85b8fc1d01cb0eed3eb3950afb9ac17
velengel <koutyan_dora_ouchi@yahoo.co.jp>
2020-10-09 11:30:20 +0000

    add unary

--- 9cc.c
+++ 9cc.c
@@ -141,6 +141,7 @@ Node *new_num(int val){
 
 Node *expr();
 Node *mul();
+Node *unary();
 Node *primary();
 
 
@@ -159,16 +160,22 @@ Node *expr(){
 }
 
 Node *mul(){
-	Node *node = primary();
+	Node *node = unary();
 
 	for(;;){
 		if(consume('*'))
-			node = new_binary(ND_MUL, node, primary());
+			node = new_binary(ND_MUL, node, unary());
 		else if(consume('/'))
-			node = new_binary(ND_DIV, node, primary());
+			node = new_binary(ND_DIV, node, unary());
 		else
 			return node;
 	}
+}
+
+Node *unary(){
+	if(consume('+')) return unary();
+	if(consume('-')) return new_binary(ND_SUB, new_num(0), unary());
+	return primary();
 }
 
 Node *primary(){
